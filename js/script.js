8e9e8d045dc704efb8b81887039fd8d9c540738c
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-06-16 15:35:56 +0200

    add title and img from vue JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 console.log("vue ok", Vue);
+
+const { createApp } = Vue;
+
+const app = createApp({
+  data() {
+    return {
+      myTitle: "ciao a tutti",
+      myImage:
+        "https://yt3.googleusercontent.com/UXFC9eFKxjbOcUeEiIFqpywdInXJijIrh5bkfUMPlKhCLKaO6rrTfh5O5IzNTS_2Wap2yBk1J44=s900-c-k-c0x00ffffff-no-rj",
+    };
+  },
+});
+
+app.mount("#root");
